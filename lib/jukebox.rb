ce024f04256d9d5416f5318e46e85dfe1860cadc
Anthony Pardo <pardo.anthony@gmail.com>
2019-11-14 19:33:32 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -55,10 +55,10 @@ def exit_jukebox()
 end
 
 def run(songs)
-  input = ""
+  puts "Please enter a command:"
+  input = gets.strip
   until input == "exit" do 
-    if input == ""
-    elsif input == "exit"
+    if input == "exit"
       exit_jukebox
     elsif input == "help"
       help
